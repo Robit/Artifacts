190f16d4b8b1a0fd0759baa5c42094b3290733fc
rm2023 <rohit.raj.mittal@gmail.com>
2020-06-13 21:50:59 -0700

    Initial commit

---
+++ src/main/java/io/github/rm2023/Artifacts/Main.java
@@ -0,0 +1,11 @@
+package io.github.rm2023.Artifacts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
+
